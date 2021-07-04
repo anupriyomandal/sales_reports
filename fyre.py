8312c3d500b2e79af0a88ddff077ec6eb684bfd5
Anupriyo Mandal <anupriyo@debian>
2021-07-04 13:43:26 +0530

    changes made

--- fyre.py
+++ fyre.py
@@ -1,26 +1,23 @@
+import csv
 import datetime
 from dt import dt
 
 def fy(PATH):
     year = int('20' + PATH[-6:-4]) - 1
     with open(PATH, 'r', encoding = 'utf-16') as f:
-        re = {}
-        co = {}
-        next(f)
+        csv_reader = csv.reader(f, delimiter = '\t')
+        next(csv_reader)
         months = [datetime.datetime(year, i, 1) for i in range(4,13)] + [datetime.datetime(year+1, i, 1) for i in range(1,4)]
-        for line in f:
-            line = line.split('\t')
-            dl = line[2]
-            ct = line[15]
-            if ct not in co:
-                co[ct] = {}
-            mt = dt(line[5])
-            val = int(line[28].replace(',', ''))
-            qt = int(line[-3].replace(',', ''))
-            if dl not in re:
-                re[dl] = [0]*12
-            if dl not in co[ct]:
-                co[ct][dl] = [0]*12
-            re[dl][months.index(mt)] += val
-            co[ct][dl][months.index(mt)] += qt
-    return re, co
+        re = {}
+        re['val'] = {}
+        f = lambda x: int(x.replace(',',''))
+        for line in csv_reader:
+            dl, ct, mt, val, qt = line[2], line[15], dt(line[5]), f(line[-2]), f(line[-3])
+            re[ct] = {}
+            if dl not in re[ct]:
+                re[ct][dl] = [0]*12
+            if dl not in re['val']:
+                re['val'][dl] = [0]*12
+            re['val'][dl][months.index(mt)] += val
+            re[ct][dl][months.index(mt)] += qt
+    return re
